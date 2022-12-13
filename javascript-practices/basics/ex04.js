b176d1efe5c2051d392b3217b1d469410a5acc53
Son-Sumin <goldkitty007@gmail.com>
2022-12-13 15:04:34 +0900

    js #4

--- javascript-practices/basics/ex04.js
+++ javascript-practices/basics/ex04.js
@@ -11,6 +11,42 @@
    - 자바와 같은 블록 스코프를 지원한다.
    - let 키워드를 붙혀서 블록 범위를 가지게 한다.
    - const 키워드는 블록 범위에 있는 상수에 적용한다.
+     const는 값이 변하지 않는 상수(한 번 할당 후 변경 불가) -> 재할당 필요시 let 선언하여 사용
+
+5. 결론
+   const/let 만 사용하고 둘 중에 하나만 반드시 붙히자!!! -> 안 붙이면 전역변수 됨(hoisting)
 
 */
 
+var i = 10;      // 전역범위
+var f = function() {
+    var i = 20;  // 블록범위
+    j = 100;     // 전역범위 (var 없으니까)
+
+    console.log(i);
+    i = j - i;
+    console.log(i);
+}
+
+f();   // f를 호출해야 f 내 함수가 실행됨
+console.log(i);
+console.log(j);
+
+console.log("===var키워드는 함수 블록에서만 함수 범위를 만든다.=========");
+if(90 + 10 === 100) {
+    var k = 20;
+}
+console.log(k);
+
+
+{
+    let x = 1000;
+    const PI = 3.15;
+
+    x = 100;
+    PI = 0;   // assigne error
+
+}
+
+// console.log(x);   // not defined error
+// console.log(PI);  // not defined error
