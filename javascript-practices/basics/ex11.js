40e5d36b3abf138a2443cc547a442bbea84c5558
Son-Sumin <goldkitty007@gmail.com>
2022-12-14 14:25:26 +0900

    arrat

--- javascript-practices/basics/ex11.js
+++ javascript-practices/basics/ex11.js
@@ -2,4 +2,65 @@
 Array 객체 함수: Array.prototype.*
 */
 
-var a = [];
+var colors = ['black', 'white', 'yellow'];
+var fruits = ['apple', 'mango', 'banana'];
+
+// concat
+console.log("========concat==========");
+var a1 = fruits.concat(colors);
+console.log(a1);
+
+// pop, push: stack 지원 함수
+console.log("========stack 지원==========");
+var color = colors.pop();
+console.log(color, colors);
+
+colors.push('red');
+console.log(colors);
+
+// join ; String을 만듦
+console.log("========join==========");
+var str = fruits. join(":");
+console.log(str)
+
+// reverse
+console.log("========reverse==========");
+console.log(fruits);
+fruits.reverse();
+console.log(fruits);
+
+// shift ; 맨 앞 요소 튕겨나감
+console.log("========shift==========");
+var numbers1 = [1000, 3000, 2000, 5000, 4000, 8000];
+var number = numbers1.shift();
+console.log(number, numbers1);
+
+// slice
+console.log("========slice==========");
+var numbers2 = numbers1.slice(1, 3);  // index 1이상 3미만
+console.log(numbers1, numbers2);
+
+// splice : 1. index에서 count개를 삭제하고, 삭제하고 남은 요소를 배열에 담아 반환
+console.log("========splice(index, count)==========");
+console.log(fruits);
+var fruits2 = fruits.splice(0,2);  // index 0에서부터 2개를 삭제 
+console.log(fruits2, fruits);
+
+// splice : 2. index에서 count개를 삭제하고 replace(요소)로 대체한 후, 삭제하고 남은 요소를 배열에 담아 반환
+console.log("========splice(index, count, replace)==========");
+var a1 = [0,1,2,3,4];
+var a2 = a1.splice(1,3,10);  // index 1부터 3개 [1,2,3]
+var a3 = a1.splice(0,1,['a', 'b']); 
+console.log(a3, a2, a1);
+
+// splice : 3. removeAt() 처럼 작동 (실제로 Array.prototype.* 에 removeAt() 없음)
+console.log("========splice - removeAt() 처럼 작동==========");
+var a1 = [0,1,2,3,4];
+a1.splice(2,1);         // a1.removeAt(2)  ->  ex12.js 확인
+console.log(a1);
+
+// splice : 4. insertAt() 처럼 작동
+console.log("========splice - insertAt() 처럼 작동==========");
+var a1 = [0,1,2,3,4];
+a1.splice(1,0,10);      // a1.insertAt(1, 10)  ->  ex12.js 확인
+console.log(a1);
---
+++ javascript-practices/basics/ex12.js
@@ -0,0 +1,65 @@
+/*
+Array 확장 (prototype 기반의 확장)
+List 함수 사용하기
+*/
+
+// TypeError: a.remove is not a function
+// var a = [1,2,4];
+// a.remove(2); 
+Array.prototype.remove = function(index) {
+    this.splice(index, 1);
+}
+
+
+Array.prototype.insert = function(index, value) {
+    if(value instanceof Array) {
+        // for(let i = 0; i < value.length; i++) {
+        //     this.splice(index+i, 0, value[i]);
+        // }
+        console.log("this: " + this);
+
+        // 오류!!
+        // 콜백함수 안의 this는 어휘 상의 this와 일치하지 않는다.
+            // forEach는 call-back 함수 넣어주기
+        // value.forEach(function(e){
+        //     console.log("this: " + this);    -> this: [object global]
+        //     this.splice(index++, 0, e);
+        // });
+
+        
+        // 해결방법1 ; foreach문 사용할때 쓰기 [변수 생성]
+        // var _this = this;
+        // value.forEach(function(e){
+        //     _this.splice(index++, 0, e);
+        // });
+
+
+        // 해결방법2 ; foreach문 사용할때 쓰기 [.bind()는 Function.prototype.*]
+        // Function.prototype.bind() 함수 사용
+        // 함수 실행 불록 안의 this를 세팅할 수 있다.
+        // 위에서 local this로 인해 오류였지만, bind(this)는 insert이기에 this.splice는 insert로 세팅
+        value.forEach(function(e){
+            this.splice(index++, 0, e);
+        }.bind(this) );
+
+    } else {
+        this.splice(index, 0, value);
+    }
+}
+
+// List 함수 사용하기
+var a = [1,2,4];
+console.log(a);  // [1,2,4]
+
+a.insert(2, 3)
+console.log(a);  // [1,2,3,4]
+
+a.remove(2); 
+console.log(a);  // [1,2,4]
+
+a.insert(2, ['a', 'b', 'c'])
+console.log(a);  // [ 1, 2, [ 'a', 'b', 'c' ], 4 ]
+// [ 1, 2, 'a', 'b', 'c', 4 ] 출력하기 위해
+// 1. Array.prototype.insert에 if문 추가
+// 2. 해결방법1 ; foreach문 사용 [변수 생성]
+// 3. 해결방법2 ; foreach문 사용 [.bind(): Function.prototype.*]
