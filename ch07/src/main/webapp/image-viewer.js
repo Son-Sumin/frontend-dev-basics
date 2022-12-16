3eacf61595ed8cd93f418167772b3e5cb77794ce
Son-Sumin <goldkitty007@gmail.com>
2022-12-16 15:06:37 +0900

    jquery #5

---
+++ ch07/src/main/webapp/image-viewer.js
@@ -0,0 +1,22 @@
+var imageViewer = {
+	images: [
+	"국화:Chrysanthemum.jpg", 
+	"사막:Desert.jpg", 
+	"수국:Hydrangeas.jpg", 
+	"해파리:Jellyfish.jpg", 
+	"코알라:Koala.jpg", 
+	"등대:Lighthouse.jpg", 
+	"펭귄:Penguins.jpg", 
+	"툴립:Tulips.jpg"],
+	init: function() {
+		$(function() {
+			// this.image로 하면 click이 작용되지 않을 수도 있음 -> 해결법 binging
+			// _images; 암묵적으로 _ 붙이면 private 의미함
+			
+			// setInterval ; 슬라이드 구현
+			// 시작 누르면 버튼 stop으로
+			// stop 누르면 시작으로 바뀌게 하기
+		});
+		
+	}
+}
